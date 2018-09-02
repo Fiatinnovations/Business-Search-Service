b57afa459fd4e332716fb067cf9bcd344fcca935
Fiatinnovations <oyinolasunkanmi@gmail.com>
2018-09-02 20:28:03 +0100

    The layouts, Business components, BusinessList Component and SearchBar components implemented

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+
+class BusinessList extends React.Component{
+	render(){
+		return(
+
+<div class="BusinessList">
+ <Business/>
+</div>
+
+			);
+	}
+}
+
+export default BusinessList;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './SearchBar.js';
+import '../BusinessList/BusinessList';
+
+
+const sortByOptions = {
+  'Best Match' : 'best_match',
+  'Highest Rated' : 'rating',
+  'Most reviewed' : 'review_count'
+};
+
+class SearchBar extends React.Component{
+  renderSortByOptions(){
+    /**To iterate through the object, you'll need to start by accessing the keys of the sortByOptionsobject.
+     Call the keys() method on the JavaScript Object library. Pass in sortByOptions as the argument.**/
+    return Object.keys(sortByOptions).map(sortByOption=>{
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key = {sortByOptionValue}>{sortByOption}</li>
+    });
+  }
+render(){
+  return(
+    <div className="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+     {this.renderSortByOptions()}
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" />
+    <input placeholder="Where?" />
+  </div>
+  <div className="SearchBar-submit">
+    <a>Lets Go</a>
+  </div>
+</div>
+  );
+}
+}
+export default SearchBar;
